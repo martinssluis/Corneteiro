c8e10387cc3845502c0388dbb94506089ad2ec07
Luis <martinsluisdev@gmail.com>
2026-02-27 09:45:10 -0300

    feat: registra blueprint para rotas de clubes na aplicação

--- app/__init__.py
+++ app/__init__.py
@@ -9,9 +9,11 @@ def create_app():
     from .routes.mercado_routers import mercado_bp
     from .routes.atletas_routers import atleta_bp
     from .routes.pontuados_routes import pontuados_bp
+    from .routes.clubes_routes import clubes_bp
     
     app.register_blueprint(mercado_bp, url_prefix="/mercado")
     app.register_blueprint(atleta_bp, url_prefix="/atletas")
     app.register_blueprint(pontuados_bp, url_prefix="/pontuados")
+    app.register_blueprint(clubes_bp, url_prefix="/clubes")
     
     return app
