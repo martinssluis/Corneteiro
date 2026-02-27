011534d0dc3516f02c63b53535ac5533887bdecd
Luis <martinsluisdev@gmail.com>
2026-02-27 09:30:51 -0300

    feat: adiciona registro do blueprint para rotas de pontuados

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,10 @@ def create_app():
     # registrar blueprints
     from .routes.mercado_routers import mercado_bp
     from .routes.atletas_routers import atleta_bp
+    from .routes.pontuados_routes import pontuados_bp
     
     app.register_blueprint(mercado_bp, url_prefix="/mercado")
     app.register_blueprint(atleta_bp, url_prefix="/atletas")
+    app.register_blueprint(pontuados_bp, url_prefix="/pontuados")
     
     return app
