467b266580b479a795dd550c73349b1567f8690a
Luis <martinsluisdev@gmail.com>
2026-03-02 14:39:46 -0300

    feat: adiciona tratamento de exceções para requisições e erros HTTP

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,33 @@
 from flask import Flask
 from .config import Config
+import requests
+from .utils.erros import resposta_erro
 
 def create_app():
-    app = Flask (__name__)
+    app = Flask(__name__)
     app.config.from_object(Config)
-    
-    # registrar blueprints
+
+
     from .routes.mercado_routers import mercado_bp
     from .routes.atletas_routers import atleta_bp
     from .routes.pontuados_routes import pontuados_bp
     from .routes.clubes_routes import clubes_bp
-    
+
     app.register_blueprint(mercado_bp, url_prefix="/mercado")
     app.register_blueprint(atleta_bp, url_prefix="/atletas")
     app.register_blueprint(pontuados_bp, url_prefix="/pontuados")
     app.register_blueprint(clubes_bp, url_prefix="/clubes")
-    
+
+    @app.errorhandler(requests.exceptions.RequestException)
+    def handle_requests_exception(e):
+        return resposta_erro("Falha ao consultar API do Cartola", status=502, detalhe=str(e))
+
+    @app.errorhandler(404)
+    def handle_404(e):
+        return resposta_erro("Recurso não encontrado", status=404)
+
+    @app.errorhandler(500)
+    def handle_500(e):
+        return resposta_erro("Erro interno do servidor", status=500, detalhe=str(e))
+
     return app
